fd9a4a5f1242d9402f5056291d11470fdeaaef1a
bcstep-vivek <bcstepit.app@gmail.com>
2026-04-16 10:13:13 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@core': path.resolve(__dirname, './src/core'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
+});
